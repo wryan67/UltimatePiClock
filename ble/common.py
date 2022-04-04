72674e1fafec2bb91722387b5b82908445b54fa2
wade ryan <wryan67@gmail.com>
2022-04-03 23:01:01 -0500

    better settings json

--- ble/common.py
+++ ble/common.py
@@ -12,3 +12,11 @@ UNIT_DESCRIPTOR_UUID = "00000003-9233-face-deaf-3e5b444bc3cf"
 TIME_DESCRIPTOR_UUID = "00000004-9233-face-deaf-3e5b444bc3cf"
 FORMAT_DESCRIPTOR_UUID = "00000005-9233-face-deaf-3e5b444bc3cf"
 
+from enum import Enum
+class JsonConversionType(Enum):
+    string = 1
+    file = 2
+
+class PrettyPrint(Enum):
+    false = 0
+    true = 1
--- ble/settings.py
+++ ble/settings.py
@@ -1,23 +1,40 @@
 
 import json
+from common import *
 
 class Settings(object):
-    unitType: str
-    timeFormat: int
-    timezone: str
 
-    def __init__(self):
-        self.unitType='C'
-        self.timzone = "Central"
-        self.timeFormat = 1
+    def __init__(self,
+                 unitType:   str='C',
+                 timeFormat: int=1,
+                 timezone:   str='Central'
+                ):
 
-
-    def __init__(self, unitType: str, timeFormat: int, timezone: str):
         self.unitType = unitType
         self.timeFormat = timeFormat
         self.timezone   = timezone
 
-    def toJson(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
+    def toJson(self, prettyPrint:PrettyPrint=PrettyPrint.false):
+        if prettyPrint == PrettyPrint.true:
+            return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+        else:
+            return json.dumps(self, default=lambda o: o.__dict__)
+
+    @staticmethod
+    def fromJson(input: str, mode: JsonConversionType = JsonConversionType.string):
+        jsonString:str
+        if mode == JsonConversionType.string:
+            jsonString = input
+        else:
+            inputFile = open(input, "r");
+            jsonString = inputFile.read()
+            inputFile.close()
+
+        thisDict=json.loads(jsonString)
+        return Settings(thisDict)
+
+settings = Settings.fromJson("settings.json", JsonConversionType.file)
 
 
+
+print("timeFormat="+str(settings.timeFormat))
